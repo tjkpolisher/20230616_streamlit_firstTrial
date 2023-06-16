3f1e1a2a14f7195902257077d872765fed941f14
tjkpolisher <jktak5609@gmail.com>
2023-06-16 10:31:06 +0900

    동영상 링크 및 서브헤더 추가

--- app.py
+++ app.py
@@ -10,7 +10,17 @@ st.write("내가 만든 Streamlit 페이지, 너를 위해 구웠지")
 
 # 기능이 실행되는 순서대로 화면에서 표현
 # 즉, 스크립트에서 위쪽 줄에 있는 것들이 먼저 실행됨
-st.video("https://youtu.be/yLDqGEngb0E") # 유튜브 등 링크를 입력하면 자동으로 띄워주는 메서드
+st.header("2023 LCK Summer Split 6월 16일 제2경기 DK vs BRO")
+st.subheader("1세트")
+st.video("https://youtu.be/HOuevBst6fw") # 유튜브 등 링크를 입력하면 자동으로 띄워주는 메서드
+st.subheader("2세트")
+st.video("https://youtu.be/QT5aaVPpHZE")
+st.subheader("3세트")
+st.video("https://youtu.be/yLDqGEngb0E")
+st.subheader("MVP 인터뷰")
+st.video("https://youtu.be/9OU_VzcO9Ag")
+st.subheader("풀 경기 영상 및 분석데스크")
+st.video("hhttps://youtu.be/3f2EyaXkfLg")
 st.image("https://cdn.pixabay.com/photo/2023/06/07/12/51/insect-8047159_1280.jpg") # 이미지 주소 입력 시 이미지 출력
 st.image("img/img.jpg") # 로컬 파일
 st.image(image="img/img.jpg")  # 키워드를 사용해서...
