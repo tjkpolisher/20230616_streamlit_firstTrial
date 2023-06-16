fe94ff4d2d25311ec30130e7c981ea9f2802fd60
tjkpolisher <jktak5609@gmail.com>
2023-06-16 10:18:39 +0900

    비디오 및 이미지 추가 실습

--- app.py
+++ app.py
@@ -8,4 +8,12 @@ st.header("수업 8일차에 만들었어요")
 st.subheader("그래도 잘 만들었죠?")
 st.write("내가 만든 Streamlit 페이지, 너를 위해 구웠지")
 
+# 기능이 실행되는 순서대로 화면에서 표현
+# 즉, 스크립트에서 위쪽 줄에 있는 것들이 먼저 실행됨
+st.video("https://youtu.be/yLDqGEngb0E") # 유튜브 등 링크를 입력하면 자동으로 띄워주는 메서드
+st.image("https://cdn.pixabay.com/photo/2023/06/07/12/51/insect-8047159_1280.jpg") # 이미지 주소 입력 시 이미지 출력
+st.image("img/img.jpg") # 로컬 파일
+st.image(image="img/img.jpg")  # 키워드를 사용해서...
+st.image("img/img.jpg", use_column_width=True)  # 파일 경로 (app.py)
+st.image("img/img.jpg", width=100)  # 파일 경로 (app.py)
 # streamlit run app.py
