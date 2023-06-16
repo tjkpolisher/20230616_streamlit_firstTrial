320a9b01972f4868322fed718c7172a0eb7c1f25
tjkpolisher <jktak5609@gmail.com>
2023-06-16 09:37:31 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# pip install streamlit
+# streamlit hello
+
+import streamlit as st
+
+st.title("나의 파이썬 웹페이지")
+st.header("수업 8일차에 만들었어요")
+st.subheader("그래도 잘 만들었죠?")
+st.write("내가 만든 Streamlit 페이지, 너를 위해 구웠지")
+
+# streamlit run app.py
